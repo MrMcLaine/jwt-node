82bd15fe381e78f533174d3dee37fbc8c70b7306
MrMcLaine <Lypovskyi@gmail.com>
2023-07-23 22:31:33 +0300

    feat/ added JWT tokens as response for successfully authorization

--- src/app.js
+++ src/app.js
@@ -1,27 +1,29 @@
 const Koa = require('koa');
 const Router = require('koa-router');
+const jwtMiddleware = require('koa-jwt');
 
 const config = require('./config');
 
 const usersModule = require('./modules/users/users');
+const authModule = require('./modules/auth/auth');
 
 function createApp() {
-  const app = new Koa();
-  const router = new Router();
-  router.get('/', ctx => {
-    ctx.body = 'ok';
-  });
+    const app = new Koa();
+    const router = new Router();
+    router.get('/', ctx => {
+        ctx.body = 'ok';
+    });
 
-  router.use('/users', usersModule.routes());
+    router.use('/users', jwtMiddleware({ secret: config.secret }), usersModule.routes());
+    router.use('/auth', authModule.routes());
+    app.use(router.allowedMethods());
+    app.use(router.routes());
 
-  app.use(router.allowedMethods());
-  app.use(router.routes());
-
-  return app;
+    return app;
 }
 
 if (!module.parent) {
-  createApp().listen(config.port);
+    createApp().listen(config.port);
 }
 
 module.exports = createApp;
--- src/config.js
+++ src/config.js
@@ -1,6 +1,8 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
 const rc = require('rc');
 
 module.exports = rc('JWT', {
-  port: process.env.PORT || 3000,
-  connection: './data',
+    port: process.env.PORT || 3000,
+    connection: './data',
+    secret: 'VERYSECRETKEY',
 });
